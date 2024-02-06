b0930809e49871c6b6edc71b7d58d426ee7ce471
Uli Raudales <fulizesrp@gmail.com>
2024-02-06 15:10:25 -0800

    added renaming sound files, and sequential and simultanious sound play

--- sound_cli.py
+++ sound_cli.py
@@ -7,11 +7,6 @@ import simpleaudio as sa
 def check_extension(sound):
     return sound.endswith(".wav")
 
-def play_sound(sound):
-    wave_obj = sa.WaveObject.from_wave_file(sound)
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
-
 def print_help():
     if len(sys.argv) > 2:
         print(f"Unrecognized command: {sys.argv[2]} Please just use the following format: -h or --help.")
@@ -28,16 +23,54 @@ def list_sounds():
     for sound in os.listdir("./sounds"):
         print(sound)
 
+play_objects = []
+
+def play_sound(sound):
+    wave_obj = sa.WaveObject.from_wave_file(sound)
+    play_obj = wave_obj.play()
+    play_objects.append(play_obj)
+
 def play_sound_arg():
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 3:
         print("Invalid number of arguments. Please use the following format: -p <sound> or --play <sound>.")
         sys.exit(1)
 
+    if sys.argv[2] == "-sm":
+        for i in range(3, len(sys.argv)):
+            sound = sys.argv[i]  # add the "sounds/" prefix to the file name
+            if check_extension(sound):
+                play_sound(sound)
+            else:
+                print("Invalid sound file format. Please use a .wav file.")
+    elif sys.argv[2] == "-sq":
+        for i in range(3, len(sys.argv)):
+            sound = sys.argv[i]
+            if check_extension(sound):
+                wave_obj = sa.WaveObject.from_wave_file(sound)
+                play_obj = wave_obj.play()
+                play_obj.wait_done()
+            else:
+                print("Invalid sound file format. Please use a .wav file.")
+                break
+    else:
+        print("Invalid command. Please use type -p -sm or -p -sq.")
+        exit(1)
+        
+def rename_sound(sound):
+    return sound.replace("_", " ").replace(".wav", "")
+
+def rename_sound_arg():
+    if len(sys.argv) < 4:
+        print("Invalid number of arguments. Please use the following format: -r <sound> <new_name> or --rename <sound> <new_name>.")
+        sys.exit(1)
+
     sound = sys.argv[2]
+    new_name = sys.argv[3]
     if check_extension(sound):
-        play_sound(sound)
+        os.rename(f"{sound}", f"{new_name}")
     else:
         print("Invalid sound file format. Please use a .wav file.")
+
 
 if __name__ == "__main__":
     commands = {
@@ -46,7 +79,9 @@ if __name__ == "__main__":
         "-l": list_sounds,
         "--list": list_sounds,
         "-p": play_sound_arg,
-        "--play": play_sound_arg
+        "--play": play_sound_arg,
+        "-r": rename_sound_arg,
+        "--rename": rename_sound_arg
     }
 
     try:
@@ -55,3 +90,7 @@ if __name__ == "__main__":
     except (IndexError, KeyError):
         print("Invalid command. Use -h or --help for help.")
         sys.exit(1)
+
+    # after all commands have been processed, wait for all sounds to finish
+    for play_obj in play_objects:
+        play_obj.wait_done()
