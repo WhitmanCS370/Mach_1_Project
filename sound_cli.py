c924936bd1bc1f7cf12c1069e2edf5963688cad9
estradam19 <143847708+estradam19@users.noreply.github.com>
2024-02-18 13:38:36 -0800

    Update sound_cli.py
    Comments

--- sound_cli.py
+++ sound_cli.py
@@ -1,41 +1,53 @@
 import os
 import sys
 
+#import command and simple audio
 import Commands as cd
 import simpleaudio as sa
 
+#Function to check if the sound file has a .wav estension 
 def check_extension(sound):
     return sound.endswith(".wav")
 
+#Function that prints help message if the arguments are incorrect
 def print_help():
     if len(sys.argv) > 2:
         print(f"Unrecognized command: {sys.argv[2]} Please just use the following format: -h or --help.")
         sys.exit(1)
 
+    #Prints the commands that are available and their descriptions
     for command, description in cd.COMMANDS.items():
         print(f"{command}: {description}")
 
+#Function to list the sounds 
 def list_sounds():
+    #check if command line arguments are greater than 2
     if len(sys.argv) > 2:
         print(f"Unrecognized command: {sys.argv[2]} Please just use the following format: -l or --list.")
         sys.exit(1)
-
+    #list the sound files in "./sounds"
     for sound in os.listdir("./sounds"):
         print(sound)
 
+#List to store play objects for each sound played
 play_objects = []
 
+#Function to play sounds 
 def play_sound(sound):
     wave_obj = sa.WaveObject.from_wave_file(sound)
     play_obj = wave_obj.play()
     play_objects.append(play_obj)
 
+#Function that handdles different comand lines to play the sounds
 def play_sound_arg():
+    #check if the command line arguments are less than 3
     if len(sys.argv) < 3:
         print("Invalid number of arguments. Please use the following format: -p <sound> or --play <sound>.")
         sys.exit(1)
 
+    #if "-sm" provided 
     if sys.argv[2] == "-sm":
+        #plays sound sequentially 
         for i in range(3, len(sys.argv)):
             sound = sys.argv[i]  # add the "sounds/" prefix to the file name
             if check_extension(sound):
@@ -43,6 +55,7 @@ def play_sound_arg():
             else:
                 print("Invalid sound file format. Please use a .wav file.")
     elif sys.argv[2] == "-sq":
+        #plays sound simultaneously 
         for i in range(3, len(sys.argv)):
             sound = sys.argv[i]
             if check_extension(sound):
@@ -55,23 +68,29 @@ def play_sound_arg():
     else:
         print("Invalid command. Please use type -p -sm or -p -sq.")
         exit(1)
-        
+
+#function renames a sound file
 def rename_sound(sound):
+    #replace "_" and ".wav" with spaces and 
     return sound.replace("_", " ").replace(".wav", "")
 
 def rename_sound_arg():
+    #check if command line arguments are less than 4
     if len(sys.argv) < 4:
         print("Invalid number of arguments. Please use the following format: -r <sound> <new_name> or --rename <sound> <new_name>.")
         sys.exit(1)
-
+        
+    #Extract the original sound file name and new name from command line
     sound = sys.argv[2]
     new_name = sys.argv[3]
+
+    #Check if file has a valid .wav extension 
     if check_extension(sound):
         os.rename(f"{sound}", f"{new_name}")
     else:
         print("Invalid sound file format. Please use a .wav file.")
 
-
+#maps command line to corresponding function
 if __name__ == "__main__":
     commands = {
         "-h": print_help,
@@ -84,6 +103,7 @@ if __name__ == "__main__":
         "--rename": rename_sound_arg
     }
 
+    #executes the function based on the command line
     try:
         command = sys.argv[1]
         commands[command]()
