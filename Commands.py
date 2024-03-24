b28fb8778bf3a85966f681c93785f54271d1be4e
Janet Davis <davisj@whitman.edu>
2024-03-24 11:52:30 -0700

    Feedback on Epoch 1

--- Commands.py
+++ Commands.py
@@ -1,6 +1,9 @@
 # COMMANDS
 # A dictionary that contains the commands and their descriptions
 
+# TODO: By convention, all Python module names should begin with a lowercase letter
+#       See https://peps.python.org/pep-0008/#package-and-module-names
+
 COMMANDS = {
     "-h or --help": "Prints all the available commands",
     "-l or --list": "Lists all the available sounds",
