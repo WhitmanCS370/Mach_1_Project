8f22fac98b9213a8a4d886382b79951151688162
halversm <halversm@whitman.edu>
2024-04-01 11:26:32 -0700

    included -rand command for random_snippets

--- Commands.py
+++ Commands.py
@@ -5,5 +5,6 @@ COMMANDS = {
     "-h or --help": "Prints all the available commands",
     "-l or --list": "Lists all the available sounds",
     "-p or --play (-sm or -sq) <sound>": "Plays a sound file, -sm plays multiple sounds at the same time, -sq plays sounds sequentially",
-    "-r or --rename <sound> <new_name>": "Renames a sound file"
+    "-r or --rename <sound> <new_name>": "Renames a sound file",
+    "-rand <sound>": "Play a random snippet of a sound file"
 }
